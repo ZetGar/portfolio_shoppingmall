9ee242ceb3d8b6cf857584dffd6d850e4c7730a1
ZetGar <88043986+ZetGar@users.noreply.github.com>
2021-09-01 10:24:23 +0900

    Update shopping.css

--- js/shopping.js
+++ js/shopping.js
@@ -7,7 +7,7 @@ $(function(){
     const $secSlide = $(".mn_slidecontainer li").eq(1);
   
     $secSlide.animate({left:0}, 1000, function(){
-      $firSlide.css({left:1000});
+      $firSlide.css({left:100+'vw'});
       $firSlide.appendTo($container);
     });
   };
@@ -15,4 +15,23 @@ $(function(){
   $(window).on('load', function(){
     setInterval(moveFn, 2000);
   });
+
+  
+  // section_product 슬라이드
+
+  const slideFn = function(){
+    const $container = $('.slide');
+    const $firSlide = $(".slide li").eq(0);
+    const $secSlide = $(".slide li").eq(1);
+
+    $secSlide.animate({bottom:0}, 1000, function(){
+      $firSlide.css({bottom:370});
+      $firSlide.appendTo($container);
+    });
+  };
+
+  $(window).on('load', function(){
+    setInterval(slideFn, 2000);
+  });
+
 });
